d28bb917ce0b1946b7dd6d086a7bd79d407e71fc
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-14 20:07:01 +0300

    Frist

---
+++ bin/dart_challenge3.dart
@@ -0,0 +1,5 @@
+import 'package:dart_challenge3/dart_challenge3.dart' as dart_challenge3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_challenge3.calculate()}!');
+}
---
+++ test/dart_challenge3_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_challenge3/dart_challenge3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
