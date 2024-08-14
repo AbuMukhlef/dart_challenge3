4ba0c4ae990bba5d102638324f004780d3a9f498
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-14 20:14:27 +0300

    Worked

--- bin/dart_challenge3.dart
+++ bin/dart_challenge3.dart
@@ -1,5 +1,3 @@
-import 'package:dart_challenge3/dart_challenge3.dart' as dart_challenge3;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_challenge3.calculate()}!');
+void main() {
+  print("Hello World!");
 }
